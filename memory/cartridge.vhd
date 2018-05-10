667b429f5c31eace597e992321938c76a9962535
Gmarin10 <gkm239@nyu.edu>
2018-05-10 09:13:24 -0400

    Add files via upload

---
+++ memory/cartridge.vhd
@@ -0,0 +1,79 @@
+
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:09:52 03/30/2018 
+-- Design Name: 
+-- Module Name:    cartridge - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--ROM based upon code given from: http://people.sabanciuniv.edu/erkays/el310/MemoryModels.pdf
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity cartridge is
+port(
+    --MEMORY RELATED
+    AB : IN STD_LOGIC_VECTOR(19 downto 0); -- ADDRESS BUS A
+    DB : OUT STD_LOGIC_VECTOR(7 downto 0); --DATA BUS
+	 CLK: IN STD_LOGIC
+);
+end cartridge;
+
+architecture Behavioral of cartridge is
+
+component rom1 is 
+PORT(
+	ENA            : IN STD_LOGIC;  --opt port
+	ADDRA          : IN STD_LOGIC_VECTOR(18 DOWNTO 0);
+	DOUTA          : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+   CLKA       : IN STD_LOGIC
+);
+end component;
+
+component rom2 is 
+PORT(
+	ENA            : IN STD_LOGIC;  --opt port
+	ADDRA          : IN STD_LOGIC_VECTOR(18 DOWNTO 0);
+	DOUTA          : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+   CLKA       : IN STD_LOGIC
+);
+end component;
+
+begin
+	romA : rom1 PORT MAP(en_A,add,out_A,clk);
+	romB : rom2 PORT MAP(en_B,add,out_B,clk); 
+	SIGNAL en_A: std_logic;
+	SIGNAL en_B: std_logic; 
+	SIGNAL add: STD_LOGIC_VECTOR(18 DOWNTO 0);
+	
+	en_A = AB(19);
+	en_B = not(AB(19));
+	add = AB(18 downto 0);
+	
+	process(add) begin
+		if(en_A = '1') then DB <= out_B; else
+		DB <= out_A; end if;
+	end process;
+	
+end Behavioral;
---
+++ memory/memory_map.vhd
@@ -0,0 +1,115 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:50:43 03/25/2018 
+-- Design Name: 
+-- Module Name:    cpu_memory_direct - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+
+--LoROM Mapping
+
+entity memory_map is
+port(
+--INPUTS
+ADDBUS : IN STD_LOGIC_VECTOR(23 downto 0); --Address Bus
+CART_1 : IN STD_LOGIC; --Does it need to access the cartridge
+WRAM_1 : IN STD_LOGIC; --Does it need to access the RAM
+
+--OUTPUTS
+AB : OUT STD_LOGIC_VECTOR(23 downto 0); -- A-Bus
+PA : OUT STD_LOGIC_VECTOR(7 downto 0) -- B-Bus (No relation to the bank signal)
+);
+end memory_map;
+
+architecture Behavioral of memory_map is
+
+SIGNAL bank : STD_LOGIC_VECTOR(7 downto 0);
+SIGNAL offset : STD_LOGIC_VECTOR(15 downto 0);
+SIGNAL internal : STD_LOGIC;
+
+begin
+bank <= ADDBUS(23 downto 17);
+process(ADDBUS) begin
+	--Mirrored Sections (Add Internal registers)
+	if((bank >= x"00" and bank <= x"3F") or (bank >= x"80" and bank <= x"BF")) then
+		if(offset >= x"0000" and offset <= x"1FFF") then
+			AB <= x"7E" & offset;
+		elsif ((offset >= x"2000" and offset <= x"20FF") or (offset >= x"2200" and offset <= x"3FFF") or (offset >= x"4400" and offset <= x"7FFF")) then --Uunused
+			AB <= bank & offset;
+		elsif (offset >= x"2100" and offset <= x"21FF") then 
+			PA <= offset(7 downto 0);
+		end if;
+	end if;	
+	
+	
+	--CARTRDIGE mapping	
+	if((bank >= x"00" and bank <= x"3F") or (bank >= x"40" and bank <= x"6F") or (bank >= x"70" and bank <= x"7D")) then
+		if(offset >= x"8000" and offset <= x"FFFF") then
+			bank(7) <= '0';
+			offset(15) <= '0';
+			AB <= bank & offset;
+		--RAM
+		elsif((offset >= x"0000" and offset <= x"7FFF") and (bank >= x"70" and bank <= x"7D")) then			
+			AB <= bank & offset;
+		end if;
+	end if;
+	
+end process; 
+
+end Behavioral;
+
+-- LOROM DOCUMENTATION: https://www.cs.umb.edu/~bazz/snes/cartridges/lorom.html#memory-map
+
+--MEMORY MAP TAKEN FROM: https://wiki.superfamicom.org/memory-mapping
+--  Banks  |  Addresses  | Speed | Mapping
+-----------+-------------+-------+---------
+-- $00-$3F | $0000-$1FFF | Slow  | Address Bus A + /WRAM (mirror $7E:0000-$1FFF)
+--         | $2000-$20FF | Fast  | Address Bus A
+--         | $2100-$21FF | Fast  | Address Bus B
+--         | $2200-$3FFF | Fast  | Address Bus A
+--         | $4000-$41FF | XSlow | Internal CPU registers (see Note 1 below)
+--         | $4200-$43FF | Fast  | Internal CPU registers (see Note 1 below)
+--         | $4400-$5FFF | Fast  | Address Bus A
+--         | $6000-$7FFF | Slow  | Address Bus A
+--         | $8000-$FFFF | Slow  | Address Bus A + /CART
+-----------+-------------+-------+---------
+-- $40-$7D | $0000-$FFFF | Slow  | Address Bus A + /CART
+-----------+-------------+-------+---------
+-- $7E-$7F | $0000-$FFFF | Slow  | Address Bus A + /WRAM
+-----------+-------------+-------+---------
+-- $80-$BF | $0000-$1FFF | Slow  | Address Bus A + /WRAM (mirror $7E:0000-$1FFF)
+--         | $2000-$20FF | Fast  | Address Bus A
+--         | $2100-$21FF | Fast  | Address Bus B
+--         | $2200-$3FFF | Fast  | Address Bus A
+--         | $4000-$41FF | XSlow | Internal CPU registers (see Note 1 below)
+--         | $4200-$43FF | Fast  | Internal CPU registers (see Note 1 below)
+--         | $4400-$5FFF | Fast  | Address Bus A
+--         | $6000-$7FFF | Slow  | Address Bus A
+--         | $8000-$FFFF | Note2 | Address Bus A + /CART
+-----------+-------------+-------+---------
+-- $C0-$FF | $0000-$FFFF | Note2 | Address Bus A + /CART
+
